24130ba7f7723f0e0d321e27c4a42d240f9023f3
unknown <rajatrajgupta19@gamil.com>
2023-05-09 01:29:26 +0530

    cors commmit

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -1,4 +1,4 @@
-const BackendUrl = 'http://localhost:8000/';
-//const BackendUrl = 'https://rajatplusmern2.onrender.com/';
+//const BackendUrl = 'http://localhost:8000/';
+const BackendUrl = 'https://rajatplusmern2.onrender.com/';
 
 export {BackendUrl};
